1b9e9318bb7ccac990edb34bf26360876d5d75fa
Elypha Rin <i@elypha.com>
2024-11-01 12:08:09 +0900

    add userscript

---
+++ release.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name        bilibili.com: Copy video cid
+// @namespace   Violentmonkey Scripts
+// @match       https://www.bilibili.com/bangumi/play/*
+// @match       https://www.bilibili.com/video/*
+// @grant       none
+// @version     1.0
+// @author      Elypha
+// @description -
+// @require     https://code.jquery.com/jquery-3.7.1.min.js
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    function doAction() {
+        let ep_id = window.location.href.match(/ep(\d+)/);
+        let episode_data = {};
+        console.log(ep_id[1]);
+        fetch(`https://api.bilibili.com/pgc/view/web/season?ep_id=${ep_id[1]}`)
+            .then(response => response.json())
+            .then(data => {
+                let episode_data = {};
+                data.result.episodes.forEach(episode => {
+                    episode_data[episode.share_copy] = episode.cid.toString().padEnd(12, ' ');
+                });
+                return episode_data;
+            })
+            .then(episode_data => {
+                let text_to_copy = Object.entries(episode_data)
+                    .map(([title, cid]) => `${cid}: ${title}`)
+                    .join('\n');
+                console.log(text_to_copy);
+                navigator.clipboard.writeText(text_to_copy);
+            });
+    }
+
+    function main() {
+        // make a copy
+        let copy = $("div[data-name='feeds']")[0].cloneNode(false);
+        copy.setAttribute("data-name", "copy");
+
+        // add a span child
+        let span = document.createElement("span");
+        span.innerText = "Copy cid";
+        copy.appendChild(span);
+
+        // create listener for click event
+        copy.addEventListener("click", () => {
+            doAction();
+        });
+
+        // add copy after the upload element
+        $("div[data-name='upload']").after(copy);
+    }
+
+    const observer = new MutationObserver(() => {
+        const element = $("div[data-name='feeds']");
+        if (element.length > 0) {
+            // Stop observing
+            observer.disconnect();
+            main();
+        }
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
